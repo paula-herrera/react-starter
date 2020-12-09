cc076f461541d0fe5d7cff1b25d234cdd2a07f26
Paula Herrera <herrera.paula5@gmail.com>
2020-12-09 12:28:27 -0700

    Created movie list that is displaying/rendering with hard-coded data

--- readme.md
+++ readme.md
@@ -3,3 +3,4 @@
 - open two tabs in terminal
 - In one tab run `npm start` (this runs live-server)
 - In the other tab run `npm run react-dev` (transpiles your React code and creates an output bundle)
+- test test
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import '../main.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render(){
     return(
     <div>
-      Starter App component - hello there
+      <div class="title"><h1>MovieList</h1></div>
+      <div class="movieList">
+          <ul>Mean Girls</ul>
+          <ul>Hackers</ul>
+          <ul>The Grey</ul>
+          <ul>Sunshine</ul>
+          <ul>Ex Machina</ul>
+      </div>
     </div>
   )}
 }

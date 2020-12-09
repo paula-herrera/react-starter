ad963b4a3cabf8de46e3b325c03b2e5478b3eca8
Paula Herrera <herrera.paula5@gmail.com>
2020-12-09 14:10:06 -0700

    Set up onChange and onClick methods for Search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,50 @@
 import React from 'react';
 
-const Search = ({handleSearchButtonClick}) => {
-  return (
-    <div>
-      <input type="text" placeholder="Search..."></input>
-      <input type="submit" value="Go!" onClick={handleSearchButtonClick}></input>
-    </div>
-  )
+// const Search = (props) => {
+//   return (
+//     <div>
+//       <input type="text" placeholder="Search..."></input>
+//       <input type="submit" value="Go!" onClick={props.handleSearchButtonClick}></input>
+//     </div>
+//   )
+// }
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: '',
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log('submit');
+  }
+
+
+  render() {
+    return (
+      <div>
+        <input type="text"
+          placeholder="Search..."
+          value={this.state.value}
+          onChange={this.handleChange}>
+        </input>
+        <input type="submit"
+          value="Go!"
+          onClick={this.handleSubmit}>
+        </input>
+      </div>
+    );
+  }
 }
 
 export default Search;

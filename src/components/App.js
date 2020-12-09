60c1487fbd414e0de3ce05336d965b0a5c78679e
Paula Herrera <herrera.paula5@gmail.com>
2020-12-09 13:38:10 -0700

    Added Search bar and set initial state to have MoviesExampleData

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,25 @@ import React from 'react';
 import '../main.css';
 import MovieList from './MovieList.js'
 import MoviesExampleData from './MoviesExampleData.js';
+import Search from './Search.js';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      movies: MoviesExampleData
+    }
   }
   render(){
     return(
     <div>
       <div className="title"><h1>MovieList</h1></div>
+      <div className="searchBar">
+        <Search />
+      </div>
       <div>
-        <MovieList movies={MoviesExampleData}/>
+        <MovieList movies={this.state.movies}/>
       </div>
     </div>
   )}
---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Search = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Search..."></input>
+      <input type="submit" value="Go!"></input>
+    </div>
+  )
+}
+
+export default Search;
